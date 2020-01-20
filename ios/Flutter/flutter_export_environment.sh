744e644bfe9e4232aae0008ff883659c9826ec6a
TheMuellenator <TheMuellenator@users.noreply.github.com>
2020-01-20 09:46:46 +0000

    Updating Android SDK, gradle wrapper, and build tool versions.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/philippmuellauer/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/philippmuellauer/Documents/Flutter/Flutter Complete/bmi-calculator"
+export "FLUTTER_APPLICATION_PATH=/Users/philippmuellauer/Downloads/BMI-Calculator-Flutter-Completed"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
