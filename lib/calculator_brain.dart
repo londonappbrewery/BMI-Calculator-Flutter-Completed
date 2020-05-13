3f0acfa36a116e7a5bb264c9f761064e1d52cfc2
newme616 <phlex616@gmail.com>
2020-05-13 23:32:51 +0200

    Added age and gender functionality
    This adds an age and gender functionality.

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -1,12 +1,16 @@
 import 'dart:math';
+import 'package:bmi_calculator/screens/input_page.dart';
 
 class CalculatorBrain {
-  CalculatorBrain({this.height, this.weight});
-
+  CalculatorBrain({this.weight, this.height, this.age, this.gender});
   final int height;
   final int weight;
-
+  final int age;
+  final Gender gender;
   double _bmi;
+  //start values for 25-34 male
+  double _overweight = 25.0;
+  double _underweight = 20;
 
   String calculateBMI() {
     _bmi = weight / pow(height / 100, 2);
@@ -14,9 +18,34 @@ class CalculatorBrain {
   }
 
   String getResult() {
-    if (_bmi >= 25) {
+    //Gender Check
+    if (gender == Gender.female) {
+      _overweight--;
+      _underweight--;
+    }
+
+    //Age Check
+    if (age <= 24) {
+      _overweight--;
+      _underweight--;
+      // 25 -34 is the normal Rate
+    } else if (age >= 35 && age <= 44) {
+      _overweight++;
+      _underweight++;
+    } else if (age >= 45 && age <= 54) {
+      _overweight = _overweight + 2;
+      _underweight = _underweight + 2;
+    } else if (age >= 55 && age <= 64) {
+      _overweight = _overweight + 3;
+      _underweight = _underweight + 3;
+    } else if (age > 64) {
+      _overweight = _overweight + 4;
+      _underweight = _underweight + 4;
+    }
+
+    if (_bmi >= _overweight) {
       return 'Overweight';
-    } else if (_bmi > 18.5) {
+    } else if (_bmi > _underweight) {
       return 'Normal';
     } else {
       return 'Underweight';
@@ -24,9 +53,9 @@ class CalculatorBrain {
   }
 
   String getInterpretation() {
-    if (_bmi >= 25) {
+    if (_bmi >= _overweight) {
       return 'You have a higher than normal body weight. Try to exercise more.';
-    } else if (_bmi >= 18.5) {
+    } else if (_bmi > _underweight) {
       return 'You have a normal body weight. Good job!';
     } else {
       return 'You have a lower than normal body weight. You can eat a bit more.';
